66d14030bbf77aedd6b4273a11863c04f12d8316
Michael <linzexinmasterchief@gmail.com>
2018-12-12 08:36:06 +0800

    explored a little about void in c++

---
+++ src/learn_cpp/learn09/learn09.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+void get_void(){
+    //error
+    #warning return type void: dont return anything
+    //return 0;
+}
+
+int main(){
+    //error
+    #warning a value of type "void" cannot be used to initialize an entity of type "int"
+    // int a = get_void();
+
+    //the code should have no output
+    return 0;
+}
