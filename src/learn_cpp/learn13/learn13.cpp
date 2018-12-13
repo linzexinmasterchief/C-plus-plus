0765dea8cd727935efe7eb6e81f94b0abde04906
Michael <linzexinmasterchief@gmail.com>
2018-12-13 09:47:39 +0800

    add more codes introducing rounding errors

--- src/learn_cpp/learn13/learn13.cpp
+++ src/learn_cpp/learn13/learn13.cpp
@@ -30,5 +30,22 @@ int main(){
     //Rule: Favor double over float unless space is at a premium,
     //as the lack of precision in a float will often lead to inaccuracies.
 
+
+
+    //Rounding errors
+    double d(0.1);
+    std::cout << d << std::endl; // use default cout precision of 6
+    std::cout << std::setprecision(17);
+    std::cout << d << std::endl;
+
+    
+    std::cout << std::setprecision(17);
+
+    double d1(1.0);
+    std::cout << d1 << std::endl; //output 1
+	
+    double d2(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1); // should equal 1.0
+    std::cout << d2 << std::endl; //output 0.99999999999999989
+
     return 0;
 }
