ab60ffe288ede56ad8a633ccf5d6ce2ea89c690d
Michael <linzexinmasterchief@gmail.com>
2018-12-13 10:14:44 +0800

    update

--- src/learn_cpp/learn09/learn09.cpp
+++ src/learn_cpp/learn09/learn09.cpp
@@ -12,7 +12,7 @@ int main(){
     //error
     #warning a value of type "void" cannot be used to initialize an entity of type "int"
     // int a = get_void();
-
+    
     //the code should have no output
     return 0;
 }
