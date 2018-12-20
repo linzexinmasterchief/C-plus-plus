fddc1236eb33d4e38cbc8afd67fc719a8348c33c
Michael <linzexinmasterchief@gmail.com>
2018-12-20 14:39:48 +0800

    tested prototype function with out implement

--- src/learn_cpp/learn04/learn04.cpp
+++ src/learn_cpp/learn04/learn04.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int add(int a, int b);
+int error();
 
 int main(){
     cout << add(1, 2) << endl;
+    //error(); will produce an error since it is not implemented
     return 0;
 }
 
