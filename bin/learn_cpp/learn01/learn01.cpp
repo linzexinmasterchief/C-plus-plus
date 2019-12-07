94f6404449817452365a3b01823921f1b0de6a55
linzexin <linzexinmasterchief@gmail.com>
2019-12-07 16:20:14 +0000

    2019/12/7

--- bin/learn_cpp/learn01/learn01.cpp
+++ bin/learn_cpp/learn01/learn01.cpp
@@ -1,7 +1,53 @@
 #include<iostream>
 
+using namespace std;
+
 int main(){
-    int x = 3;
-    std::cout << x << std::endl;
+    // for loop
+    // i=0, i++ => i=i+1
+    cout << "for loop:" << endl;
+    for (int i = 0; i <= 5; i++) {
+        cout << "Hello World" << endl;
+    }
+
+    cout << "-----------------------" << endl;
+
+    // while loop
+    cout << "while loop:" << endl;
+    int i = 0;
+    int j = 1;
+    while (i <= 5 && j <= 3) {
+        cout << "Hello World" << endl;
+        i ++;
+        j ++;
+    }
+
+    cout << "-----------------------" << endl;
+
+    // if statement
+    // if..else if...else
+    int x = 0;
+    int y = 1;
+    if (x > 0 && y > 0) {
+        cout << "larger than 0" << endl;
+    } else if (x < 0 || y < 0) {
+        cout << "not larger than 0" << endl;
+    } else {
+        cout << "other" << endl;
+    }
+
+    int a = 0;
+    while (a < 10) {
+        int b = 0;
+        while (b < 20) {
+            if (a >= b) {
+                cout << a << " x " << b << " = " << a * b << " | ";
+            }
+            b ++;
+        }
+        cout << "" << endl;
+        a ++;
+    }
+    
     return 0;
 }
--- src/learn_cpp/learn02/learn02.cpp
+++ src/learn_cpp/learn02/learn02.cpp
@@ -1,12 +1,44 @@
 #include<iostream>
+#include<string>
 
-//created a function
-void print(const int &x){
-    std::cout << x << std::endl;
-}
+using namespace std;
+class Human{
+    public:
+        string name;
+        Human(){
+            name = "none";
+        }
+        Human(string _name){
+            name = _name;
+        }
+
+        void Say_Name(){
+            Say(name);
+        }
+    private:
+        string p_string;
+        void Say(string a) {
+            cout << a << endl;
+        }
+};
+
+class Asian : public Human{
+    public:
+        Asian(string _name) : Human(){
+            name = _name;
+        }
+};
 
 int main(){
-    int x = 3;
-    print(x);
+    Human h1("A");
+    Human h2("B");
+    
+    Asian a1("C");
+
+    h1.Say_Name();
+    h2.Say_Name();
+
+    a1.Say_Name();
+
     return 0;
 }
